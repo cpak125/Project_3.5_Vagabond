db4adb12edfe47aa8505674d11ed43012c515016
Chris Pak <chris.pak125@gmail.com>
2020-07-30 21:09:27 -0400

    updated dependencies

--- node_modules/semantic-ui-react/src/addons/Pagination/PaginationItem.js
+++ node_modules/semantic-ui-react/src/addons/Pagination/PaginationItem.js
@@ -10,44 +10,8 @@ import MenuItem from '../../collections/Menu/MenuItem'
  * An item of a pagination.
  */
 class PaginationItem extends Component {
-  static propTypes = {
-    /** A pagination item can be active. */
-    active: PropTypes.bool,
-
-    /** A pagination item can be disabled. */
-    disabled: PropTypes.bool,
-
-    /**
-     * Called on click.
-     *
-     * @param {SyntheticEvent} event - React's original SyntheticEvent.
-     * @param {object} data - All props.
-     */
-    onClick: PropTypes.func,
-
-    /**
-     * Called on key down.
-     *
-     * @param {SyntheticEvent} event - React's original SyntheticEvent.
-     * @param {object} data - All props.
-     */
-    onKeyDown: PropTypes.func,
-
-    /** A pagination should have a type. */
-    type: PropTypes.oneOf([
-      'ellipsisItem',
-      'firstItem',
-      'prevItem',
-      'pageItem',
-      'nextItem',
-      'lastItem',
-    ]),
-  }
-
   handleClick = (e) => {
-    const { type } = this.props
-
-    if (type !== 'ellipsisItem') _.invoke(this.props, 'onClick', e, this.props)
+    _.invoke(this.props, 'onClick', e, this.props)
   }
 
   handleKeyDown = (e) => {
@@ -67,8 +31,9 @@ class PaginationItem extends Component {
     return MenuItem.create(this.props, {
       defaultProps: {
         active,
-        disabled,
         'aria-current': active,
+        'aria-disabled': disabled,
+        disabled,
         onClick: this.handleClick,
         onKeyDown: this.handleKeyDown,
         tabIndex: disabled ? -1 : 0,
@@ -78,6 +43,40 @@ class PaginationItem extends Component {
   }
 }
 
-PaginationItem.create = createShorthandFactory(PaginationItem, content => ({ content }))
+PaginationItem.propTypes = {
+  /** A pagination item can be active. */
+  active: PropTypes.bool,
+
+  /** A pagination item can be disabled. */
+  disabled: PropTypes.bool,
+
+  /**
+   * Called on click.
+   *
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onClick: PropTypes.func,
+
+  /**
+   * Called on key down.
+   *
+   * @param {SyntheticEvent} event - React's original SyntheticEvent.
+   * @param {object} data - All props.
+   */
+  onKeyDown: PropTypes.func,
+
+  /** A pagination should have a type. */
+  type: PropTypes.oneOf([
+    'ellipsisItem',
+    'firstItem',
+    'prevItem',
+    'pageItem',
+    'nextItem',
+    'lastItem',
+  ]),
+}
+
+PaginationItem.create = createShorthandFactory(PaginationItem, (content) => ({ content }))
 
 export default PaginationItem
